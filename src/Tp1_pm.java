c44adb7b26c30265fcb7b0526b39cbcec885c249
Gustavo Pereira <ghapereira@gmail.com>
2015-04-04 02:32:38 -0300

    Testando abertura dos arquivos em loop. Considero primeiramente uma ordem definida de abertura de arquivos. Criada estrutura para criação dos pesquisadores. Achei que a main ficou um pouco cheia, mas no caráter de teste está funcional. A verificação foi através de um println simples - ela cria cada pesquisador de acordo com sua especialidade.

--- src/Tp1_pm.java
+++ src/Tp1_pm.java
@@ -1,42 +1,93 @@
 import VeiculosDePublicacao.*;
 import Pesquisadores.*;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 
 public class Tp1_pm {
 
+    public static final int NUMERO_DE_ARQUIVOS = 6;
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Graduado g = new Graduado(1, 6, 9);
+        File[] entradas = new File[NUMERO_DE_ARQUIVOS];
+        ArrayList<Pesquisador> pesquisadores = new ArrayList<Pesquisador>();
 
-        System.out.println(g.popularidade());
+        // Loop de abertura de arquivos
+        for (int i = 0; i < NUMERO_DE_ARQUIVOS; i++) {
+            try{
+                entradas[i] = new File(args[i]);
+            }catch(ArrayIndexOutOfBoundsException e){
 
-        Mestre m = new Mestre(1, 6, 9, 1);
-        System.out.println(m.popularidade());
+                System.out.println(
+                        "Foram passados " + args.length + " arquivos de " +
+                        Tp1_pm.NUMERO_DE_ARQUIVOS +
+                        " necessarios. O programa sera encerrado.");
 
-        Doutor d = new Doutor(1, 6, 9, 1, 1, 1);
-        System.out.println(d.popularidade());
-
-        File[] entradas = new File[6];
-        for (int i = 0; i < 6; i++) {
-            entradas[i] = new File(args[i]);
+                System.exit(1);
+            }
         }
 
         Scanner fs = null;
 
-        try {
-            for (int i = 0; i < 6; i++) {
+        // Loop de processamento de arquivos
+        // Vamos considerar uma certa ordem definida de parametros?
+        // Por hora: pesquisadores.txt -> veiculos.txt -> artigos_veiculos.txt
+        // -> grafo_pesquisadores.txt -> grafo_artigos_pesquisadores.txt -> grafo_citacoes.txt
+        for (int i = 0; i < NUMERO_DE_ARQUIVOS; i++) {
+            try {
                 fs = new Scanner(entradas[i]);
-                while(fs.hasNextLine()){
-                    System.out.println(fs.nextLine());
+
+                switch (i){
+                    case 0: // pesquisadores
+                        while(fs.hasNextLine()){
+                            String[] parametros = fs.nextLine().split(";");
+                            switch (parametros[1]){
+                                case "G":
+                                    Graduado g = new Graduado(Integer.parseInt(parametros[0]),
+                                                              Integer.parseInt(parametros[2]),
+                                                              Integer.parseInt(parametros[3]));
+
+                                    pesquisadores.add(g);
+                                    break;
+
+                                case "M":
+                                    Mestre m = new Mestre(Integer.parseInt(parametros[0]),
+                                                          Integer.parseInt(parametros[2]),
+                                                          Integer.parseInt(parametros[3]),
+                                                          Integer.parseInt(parametros[4]));
+
+                                    pesquisadores.add(m);
+                                    break;
+
+                                case "D":
+                                    Doutor d = new Doutor(Integer.parseInt(parametros[0]),
+                                                          Integer.parseInt(parametros[2]),
+                                                          Integer.parseInt(parametros[3]),
+                                                          Integer.parseInt(parametros[4]),
+                                                          Integer.parseInt(parametros[5]),
+                                                          Integer.parseInt(parametros[6]));
+
+                                    pesquisadores.add(d);
+                                    break;
+                            }
+                        }
+                        break;
+
                 }
+
+            }catch (FileNotFoundException e){
+                System.out.println("Arquivo " + entradas[i] + " nao encontrado");
             }
-        }catch (FileNotFoundException e){
-            System.out.println("Arquivo pesquisadores.txt nao encontrado");
         }
-    }
+
+        for (Pesquisador p : pesquisadores){
+            System.out.println(p);
+        }
+
+    } // End main
 }
--- src/VeiculosDePublicacao/Artigo.java
+++ src/VeiculosDePublicacao/Artigo.java
@@ -4,4 +4,9 @@ package VeiculosDePublicacao;
  * Created by ghapereira on 3/26/15.
  */
 public class Artigo {
+    private int quantidadeDeCitacoes;
+
+    public void incrementaQuantidadeDeCitacoes(){
+        this.quantidadeDeCitacoes++;
+    }
 }
