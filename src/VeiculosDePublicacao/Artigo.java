b7b27af92cc22dd32b643adbbf26da89c8b2c151
André Taiar <taiar@hotmail.com>
2015-04-05 18:36:30 -0300

    Calculando qualidade do Artigo.

--- src/VeiculosDePublicacao/Artigo.java
+++ src/VeiculosDePublicacao/Artigo.java
@@ -1,12 +1,30 @@
 package VeiculosDePublicacao;
 
-/**
- * Created by ghapereira on 3/26/15.
- */
 public class Artigo {
+    
+    private int id;
+
     private int quantidadeDeCitacoes;
+
+    private VeiculoDePublicacao veiculo;
+
+    public Artigo(int id) {
+      this.id = id;
+    }
 
     public void incrementaQuantidadeDeCitacoes(){
         this.quantidadeDeCitacoes++;
+    }
+
+    public VeiculoDePublicacao getVeiculoDePublicacao() {
+      return this.veiculo;
+    }
+
+    public void setVeiculoDePublicacao(VeiculoDePublicacao v) {
+      this.veiculo = v;
+    }
+
+    public float qualidade() {
+      return this.veiculo.fatorDeImpacto() * this.quantidadeDeCitacoes;
     }
 }
